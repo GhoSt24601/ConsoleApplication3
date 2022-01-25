2c0e0f9880dffb5f495b52e31f7d6ccd6afbb3c9
YourLifeForAiur <r320stwg@yandex.ru>
2022-01-25 20:29:18 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/CarFactory.h
@@ -0,0 +1,16 @@
+#pragma once
+class CarFactory
+{
+    private:
+        static int counter;
+        void Create()
+        {
+            
+        }
+        void Count()
+        {
+            counter = 0;
+        }
+    
+};
+
